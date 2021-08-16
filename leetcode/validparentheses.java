3658abec671a44a83e63a7c6aee726231e2d6655
Bill Li <bill.li@sjsu.edu>
2021-08-15 21:20:37 -0700

    first commit

---
+++ leetcode/validparentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() < 2){
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        map.put('}','{');
+        map.put(']','[');
+        map.put(')','(');
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char letter = s.charAt(i);
+            if(letter == '(' || letter == '{' || letter == '['){
+                stack.add(letter);
+            }
+            else if(stack.isEmpty()) return false;
+            else if(stack.peek() == map.get(letter)){
+                stack.pop();
+            }
+            else
+                return false;
+        }
+        if(!stack.isEmpty())
+            return false;
+        
+        return true;
+        
+        
+    }
+}
